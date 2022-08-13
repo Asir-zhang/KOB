49cff86528c43a4befe7a50e87ea396d79a11a89
Asir <zpeng791@gmail.com>
2022-08-13 08:35:50 +0800

    创建了WEb工程，并且使用了VUE3与springboot进行了初步的前后端分离开发

---
+++ web/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+
+    <div>Bot昵称 :{{ bot_name }}</div>
+    <div>Bot战力 :{{ bot_rating }}</div>
+  </div>
+  <router-view></router-view>
+</template>
+
+
+<script>
+import $ from 'jquery'
+import { ref } from 'vue';
+
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    // 访问后端
+    $.ajax({
+      url: "http://localhost:8081/pk/get_bot_info/",
+      type: "get",
+      success: resp => {
+        // console.log(resp);
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  background: url(@/assets/back.png);
+  background-size: cover;
+}
+</style>
